02a7821cfba0cbc6dbcf8047ebdf8afce08f735c
beom-yun <scds0326@naver.com>
2024-06-27 20:19:42 +0900

    Detail Page 1

--- app/person/[id]/page.tsx
+++ app/person/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { API_URL } from "@/lib/constants";
 import { IPersonDetail } from "@/lib/types";
+import { PhotoIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
@@ -8,8 +10,12 @@ export async function generateMetadata({ params }: { params: { id: string } }) {
 }
 
 async function getPersonDetail(id: string): Promise<IPersonDetail> {
-  const response = await fetch(`${API_URL}/person/${id}`);
-  return await response.json();
+  try {
+    const response = await fetch(`${API_URL}/person/${id}`);
+    return await response.json();
+  } catch {
+    return notFound();
+  }
 }
 
 export default async function DetailPage({
@@ -17,11 +23,35 @@ export default async function DetailPage({
 }: {
   params: { id: string };
 }) {
-  try {
-    const person = await getPersonDetail(params.id);
-  } catch {
-    return notFound();
-  }
+  const person = await getPersonDetail(params.id);
 
-  return <h1>Detail Page {params.id}</h1>;
+  return (
+    <div className="flex w-full flex-col items-center gap-5 px-5 py-20 text-white xl:max-w-screen-xl">
+      <div className="flex size-72 items-center justify-center overflow-hidden rounded-lg bg-gray-600 shadow-md">
+        {person.squareImage.includes("undefined") ? (
+          <PhotoIcon className="size-40 text-gray-500" />
+        ) : (
+          <Image
+            src={person.squareImage}
+            alt={person.name}
+            width={288}
+            height={288}
+            className="object-cover"
+          />
+        )}
+      </div>
+      <span className="py-5 text-5xl font-bold">{person.name}</span>
+      <span className="text-xl">
+        Networth: {Math.round(person.netWorth / 1000)} Billion
+      </span>
+      <span className="text-xl">Country: {person.country}</span>
+      <span className="text-xl">Industry: {person.industries[0]}</span>
+      <ul className="py-5">
+        {person.bio.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
+      {/* person.financialAssets */}
+    </div>
+  );
 }
